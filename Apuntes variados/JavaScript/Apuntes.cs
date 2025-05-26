b2ae58b8163b7ced915144ef59c700c424ff9df5
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-05-26 21:00:28 +0200

    Comienzo de curso de React y de JavaScript

---
+++ Apuntes variados/JavaScript/Apuntes.cs
@@ -0,0 +1,8 @@
+// Apuntes del curso JavaScript https://www.youtube.com/watch?v=Z34BF9PCfYg&t=106s
+
+//Basico
+string nombres = "Entrecomillar con "",'' y ``"+"\n"+"El ultimo tiene la peculiaridad de poder incrustar valores, `Cuesta ${x}`";
+string calculo = "Son estandar, con ** para elevar";
+string diferenciaDeBooleanos = "con operadores devuelve directamente booleanos el de igual es === y falso es !==";
+string operadores = "&& este operador necesita que todo sea true para devolver true, es igual que en C#, tambien || que es o(si todo es true devuelve true). Tambien esta "!" que invierte el valor del bool ";
+string valoresVacios = "Tienes el valor null y undefined, null no existe y undefined no esta definido," //Undefined debe ser ampliado si no se amplia en el curso
