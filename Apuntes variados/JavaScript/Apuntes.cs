7cb1498329cdd63b5749a94bffa6499dbaae812b
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-05-27 16:39:08 +0200

    Terminado curso demasiado basico de Javascript

--- Apuntes variados/JavaScript/Apuntes.cs
+++ Apuntes variados/JavaScript/Apuntes.cs
@@ -5,4 +5,20 @@ string nombres = "Entrecomillar con "",'' y ``"+"\n"+"El ultimo tiene la peculia
 string calculo = "Son estandar, con ** para elevar";
 string diferenciaDeBooleanos = "con operadores devuelve directamente booleanos el de igual es === y falso es !==";
 string operadores = "&& este operador necesita que todo sea true para devolver true, es igual que en C#, tambien || que es o(si todo es true devuelve true). Tambien esta "!" que invierte el valor del bool ";
-string valoresVacios = "Tienes el valor null y undefined, null no existe y undefined no esta definido," //Undefined debe ser ampliado si no se amplia en el curso
+string valoresVacios = "Tienes el valor null y undefined, null no existe y undefined no esta definido,"; //Undefined debe ser ampliado si no se amplia en el curso
+string bitn = "Para evitar que te abrevie o similar un numero alto añade una n al final";
+string symbol = "Los simbolos iempre que se crean son unicos";//Si no se amplia en el curso, ampliar por mi cuenta
+string variables = "let da la orden para crear y guardar una vinculacion de una variable que puede ser modificado.\n const es igual pero la variable no es modificables en el tiempo \n Algo muy interesante es que estas variables pueden ser string, int,..."+
+    " En cambio para sumar con asignado (+=) y similares funciona igual que C# salvo que con (++) en el momento en el que lo imprime si esta detras sale antes del ++ pero queda hecho para blucles y similares";
+string Operaciones = "if:  Funcionan igual que en C# \n" +
+    "while: Funciona igual pero su estructura es distinta \n" +
+    /*
+     while (day < 4) {
+       "pepe"            //lo que hace
+       day = day + 1     //el aumento para que no sea infinito
+     }     
+     */
+    "for: Funciona igual que en C# y mantiene la misma estructura";
+string comentarios = "Como en C# // o /**/";
+string funciones = "crear funciones es muy similar (function x() {100+20}) si quieres que la consola devuelva algo añadele return. Al poner x te saldra la funcion, si pongo x() se ejecuta.\n"+
+    "Tambien se puede poner un parametro entre (), para luego añadir argumento"
