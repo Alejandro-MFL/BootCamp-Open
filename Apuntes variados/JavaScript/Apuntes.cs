ad1b8888b34f822a36b10505961aa1351426fe58
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-05-27 17:12:55 +0200

    Añadido una pagina de apuntes/curso mucho mejor

--- Apuntes variados/JavaScript/Apuntes.cs
+++ Apuntes variados/JavaScript/Apuntes.cs
@@ -1,4 +1,4 @@
-// Apuntes del curso JavaScript https://www.youtube.com/watch?v=Z34BF9PCfYg&t=106s
+// Apuntes del curso JavaScript https://www.youtube.com/watch?v=Z34BF9PCfYg&t=106s (Es muy basico, para dudas y similares mejor ir a su "curso" https://www.aprendejavascript.dev/ )
 
 //Basico
 string nombres = "Entrecomillar con "",'' y ``"+"\n"+"El ultimo tiene la peculiaridad de poder incrustar valores, `Cuesta ${x}`";
@@ -10,7 +10,21 @@ string bitn = "Para evitar que te abrevie o similar un numero alto añade una n a
 string symbol = "Los simbolos iempre que se crean son unicos";//Si no se amplia en el curso, ampliar por mi cuenta
 string variables = "let da la orden para crear y guardar una vinculacion de una variable que puede ser modificado.\n const es igual pero la variable no es modificables en el tiempo \n Algo muy interesante es que estas variables pueden ser string, int,..."+
     " En cambio para sumar con asignado (+=) y similares funciona igual que C# salvo que con (++) en el momento en el que lo imprime si esta detras sale antes del ++ pero queda hecho para blucles y similares";
-string Operaciones = "if:  Funcionan igual que en C# \n" +
+string Operaciones = "if:  Funcionan igual que en C#, muy muy similar esta switch \n" +
+    /*
+        const dia = "lunes"
+
+        switch (dia) {
+          case "lunes":
+            console.log("¡Hoy es lunes!")
+            break
+
+          default:
+            console.log("No es lunes, YAY!")
+            break
+            }
+     */
+
     "while: Funciona igual pero su estructura es distinta \n" +
     /*
      while (day < 4) {
@@ -20,5 +34,6 @@ string Operaciones = "if:  Funcionan igual que en C# \n" +
      */
     "for: Funciona igual que en C# y mantiene la misma estructura";
 string comentarios = "Como en C# // o /**/";
-string funciones = "crear funciones es muy similar (function x() {100+20}) si quieres que la consola devuelva algo añadele return. Al poner x te saldra la funcion, si pongo x() se ejecuta.\n"+
-    "Tambien se puede poner un parametro entre (), para luego añadir argumento"
+string funciones = "crear funciones es muy similar (function x() {100+20}) si quieres que la consola devuelva algo añadele return. Al poner x te saldra la funcion, si pongo x() se ejecuta.\n" +
+    "Tambien se puede poner un parametro entre (), para luego añadir argumento";
+
